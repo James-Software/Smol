9dc74b49d5010ac1b57b4d7d523cf583459476a9
James <yeedoreal@gmail.com>
2026-06-06 14:16:10 -0700

    Fredoka added, dark mode default, page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 export default function Home() {
   return (
-    <h1 className = "font-fredoka font-bold">SMOL</h1>
+    <>
+      <h1 className="font-fredoka font-bold text-9xl text-amber-600 pt-8">SMOL</h1>
+      <h2 className=""></h2>
+      <div>
+
+      </div>
+    </>
   );
 }
